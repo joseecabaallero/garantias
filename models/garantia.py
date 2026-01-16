5014dcb3a7625306b0d3f7a0bfa80113ea3720f4
José David <jcabmar2012@g.educaand.es>
2026-01-16 12:15:16 +0100

    garantia.py editado

--- models/garantia.py
+++ models/garantia.py
@@ -1,7 +1,6 @@
-# garantias/models/garantia.py
 from odoo import models, fields, api
 from datetime import timedelta
-class GarantiaProducto(models.Model):
+
 class GarantiaProducto(models.Model):
     _name = 'garantia.producto'
     _order = 'number asc'
@@ -18,28 +17,27 @@ class GarantiaProducto(models.Model):
         store=True
     )
 
-
     @api.model
     def create(self, vals):
         # Lógica para calcular el número de garantía automáticamente
         last_number = self.search([], order="number desc", limit=1).number
-        vals['number'] = last_number + 1 if last_number else 1 # Si no hay registros, empieza con 1
+        vals['number'] = last_number + 1 if last_number else 1
         return super(GarantiaProducto, self).create(vals)
 
-@api.depends('purchase_date')
+    @api.depends('purchase_date')
     def _compute_warranty_date(self):
         for record in self:
             if record.purchase_date:
                 # Sumamos 2 años (730 días) directamente a la fecha de compra
                 record.warranty_date = record.purchase_date + timedelta(days=730)
             else:
-                # Si no hay fecha de compra, el campo se deja vacío
                 record.warranty_date = False
 
-            @api.depends('warranty_date')
-            def _compute_status(self):
-                for record in self:
-                    if record.warranty_date and record.warranty_date >= fields.Date.today():
-                        record.status = 'valid'
-                    else:
-                        record.status = 'expired'
+    @api.depends('warranty_date')
+    def _compute_status(self):
+        today = fields.Date.context_today(self)
+        for record in self:
+            if record.warranty_date and record.warranty_date >= today:
+                record.status = 'valid'
+            else:
+                record.status = 'expired'
